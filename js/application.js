36070e848a2f76c5ae07559b01923cbbdfc84825
Tohar <toharrahima@hilma.tech>
2024-10-14 16:27:00 +0300

    js app

---
+++ js/application.js
@@ -0,0 +1,15 @@
+const loginPageTemplate = document.getElementsByTagName("template")[0]
+const registerPageTemplate = document.getElementsByTagName("template")[1]
+const mainPageTemplate = document.getElementsByTagName("template")[2]
+
+function loadPage(template){
+    let clon = temp.content.cloneNode(true);
+    document.body.appendChild(clon);
+}
+
+function showappContent() {
+    let clon = temp.content.cloneNode(true);
+    document.body.appendChild(clon);
+  }
+  
+showregisterContent();
